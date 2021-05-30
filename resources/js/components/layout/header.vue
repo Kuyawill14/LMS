80530fab21b207e1bb70c0c13ddc473f6c4b185c
Mwill14 <62843284+Mwill14@users.noreply.github.com>
2021-05-30 10:57:44 +0800

    sasasaasas

--- resources/js/components/layout/header.vue
+++ resources/js/components/layout/header.vue
@@ -1,5 +1,5 @@
 <template>
- <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+ <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="primary" dark>
                 <v-app-bar-nav-icon @click.stop="$emit('toggleSidebar')"></v-app-bar-nav-icon>
                 <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
                     <span class="hidden-sm-and-down">Google Contacts</span>
@@ -10,9 +10,10 @@
                 <v-btn icon>
                     <v-icon>mdi-apps</v-icon>
                 </v-btn>
-                <v-btn icon>
-                    <v-icon>mdi-bell</v-icon>
-                </v-btn>
+             
+                   <notifications></notifications>
+                 <!--    <v-icon>mdi-bell</v-icon> -->
+        
                 <v-btn icon large>
                     <v-avatar size="32px" item>
                         <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify"></v-img>
