e54bbc6a4880ed964e0b69a29cbe52180bbf1615
Mwill14 <62843284+Mwill14@users.noreply.github.com>
2021-08-22 17:34:43 +0800

    sasa

--- public/js/resources_js_components_course-view_tabs_classwork-tab_classworkListType_classworkListTeacher_vue.js
+++ public/js/resources_js_components_course-view_tabs_classwork-tab_classworkListType_classworkListTeacher_vue.js
@@ -166,12 +166,6 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
-//
-//
-//
-//
-//
-//
 var deleteDialog = function deleteDialog() {
   return __webpack_require__.e(/*! import() */ "resources_js_components_course-view_tabs_classwork-tab_dialogs_deleteDiaglog_vue").then(__webpack_require__.bind(__webpack_require__, /*! ../dialogs/deleteDiaglog */ "./resources/js/components/course-view/tabs/classwork-tab/dialogs/deleteDiaglog.vue"));
 };
@@ -383,35 +377,6 @@ var render = function() {
   return _c(
     "div",
     [
-      _c(
-        "v-dialog",
-        {
-          attrs: { persistent: "", "max-width": "600" },
-          model: {
-            value: _vm.dialog,
-            callback: function($$v) {
-              _vm.dialog = $$v
-            },
-            expression: "dialog"
-          }
-        },
-        [
-          _vm.dialog
-            ? _c("newClassworkModal", {
-                on: {
-                  CloseDialog: function($event) {
-                    _vm.dialog = !_vm.dialog
-                  },
-                  realodClassworks: function($event) {
-                    _vm.$emit("reloadClassworks"), (_vm.dialog = !_vm.dialog)
-                  }
-                }
-              })
-            : _vm._e()
-        ],
-        1
-      ),
-      _vm._v(" "),
       _vm.ClassworkLength == 0
         ? _c(
             "v-row",
