45b4e14af99406e8831b5cadb2ff39a58d1b9013
markishere16 <markjoshua.mandigma@gmail.com>
2021-09-15 19:01:22 +0800

    update

--- public/supertest/test.php
+++ public/supertest/test.php
@@ -58,7 +58,8 @@
         function createPicker() {
             if (pickerApiLoaded && oauthToken) {
                 var view = new google.picker.View(google.picker.ViewId.DOCS);
-                view.setMimeTypes("image/png,image/jpeg,image/jpg");
+                view.setMimeTypes("application/vnd.google-apps.folder");
+
                 var picker = new google.picker.PickerBuilder()
                     .enableFeature(google.picker.Feature.NAV_HIDDEN)
                     .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
