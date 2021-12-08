60746dc265a587341573e5264888c4ca115c15c5
Mwill14 <62843284+Mwill14@users.noreply.github.com>
2021-12-08 09:02:27 +0800

    updates

--- public/js/resources_js_components_Classwork_View_tabs_submissionType_check-submission_check-objective_v-5caf841.js
+++ public/js/resources_js_components_Classwork_View_tabs_submissionType_check-submission_check-objective_v-5caf841.js
@@ -494,24 +494,6 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 
 var resetConfirmation = function resetConfirmation() {
