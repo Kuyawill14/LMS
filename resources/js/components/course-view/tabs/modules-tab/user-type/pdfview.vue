2208fb9f4c29191c787e06bff608a64b0a5825fa
markishere16 <markjoshua.mandigma@gmail.com>
2022-02-20 02:27:08 +0800

    update

--- resources/js/components/course-view/tabs/modules-tab/user-type/pdfview.vue
+++ resources/js/components/course-view/tabs/modules-tab/user-type/pdfview.vue
@@ -3,10 +3,14 @@
 
 
     <v-card style="height: 620px;">
-        
-       <!-- <iframe title="google pdf viewer"  class="pdf-viewer" :src="'https://docs.google.com/viewer?embedded=true&amp;url=' + pdf_path" sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+
+        <!-- <iframe title="google pdf viewer"  class="pdf-viewer" :src="'https://docs.google.com/viewer?embedded=true&amp;url=' + pdf_path" sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
                              style="position: absolute; top: 0px; left: 0px; width: 100% !important; height: 100%; !important"></iframe> -->
-               <iframe  height="100%" class="pdf-viewer holds-the-iframe"  width=100% :src="`${pdf_path}`" ></iframe>               
+        <!-- <iframe height="100%" class="pdf-viewer holds-the-iframe" width=100% :src="`${pdf_path}`"></iframe> -->
+          <!-- <vue-pdf-app :pdf="pdf_file"></vue-pdf-app> -->
+{{pdf_file}}
+  <pdf :src="pdf_file"></pdf>
+
     </v-card>
 
 
@@ -15,22 +19,30 @@
 </template>
 
 <script>
-
+    import VuePdfApp from "vue-pdf-app";
+    // import this to use default icons for buttons
+    import "vue-pdf-app/dist/icons/main.css";
+import pdf from 'vue-pdf'
 
     export default {
         props: ['pdf_file', 'title'],
+        components: {
+            VuePdfApp,
+            pdf
+        },
+
         data() {
             return {
                 dialog: false,
-                pdf_path:null,
+                pdf_path: null,
             };
         },
 
         mounted() {
-        var host = window.location.protocol + "//" + window.location.host;
-       
-       this.pdf_path = host+ this.pdf_file;
-         this.pdf_path  =  window.location.origin+'/lib/web/viewer.html?file='+ this.pdf_file; 
+            var host = window.location.protocol + "//" + window.location.host;
+
+            this.pdf_path = host + this.pdf_file;
+            this.pdf_path = window.location.origin + '/lib/web/viewer.html?file=' + this.pdf_file;
 
         },
     };
@@ -38,15 +50,18 @@
 </script>
 
 
-<style >
-.holds-the-iframe {
-  background:url('https://orangestr.sgp1.digitaloceanspaces.com/loading/loading/Infinity-1.5s-200px.gif') center center no-repeat;
-}
+<style>
+    .holds-the-iframe {
+        background: url('https://orangestr.sgp1.digitaloceanspaces.com/loading/loading/Infinity-1.5s-200px.gif') center center no-repeat;
+    }
+
     .toolbar {
         z-index: 1 !important;
     }
+
     .pdf-viewer {
         height: 100% !important;
         width: 100% !important;
     }
+
 </style>
