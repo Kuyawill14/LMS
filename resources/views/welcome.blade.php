692cec9acb4c9c9ab05854c4c52433b1005bf0a9
Mwill14 <62843284+Mwill14@users.noreply.github.com>
2022-03-01 21:03:26 +0800

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,9 +74,9 @@
     window.location = window.location.href.replace("www.","");
 }
 
-/* setInterval(() => {
+setInterval(() => {
   console.clear();
-}, 10000); */
+}, 10000);
             </script> 
         </body>
 </html>
