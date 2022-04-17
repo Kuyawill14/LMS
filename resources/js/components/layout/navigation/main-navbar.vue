2c4bab416db958c2a8bdd540cc170b5ff40e1af8
markishere16 <markjoshua.mandigma@gmail.com>
2022-04-18 00:37:25 +0800

    update

--- resources/js/components/layout/navigation/main-navbar.vue
+++ resources/js/components/layout/navigation/main-navbar.vue
@@ -32,7 +32,7 @@
                 </v-list-item-action>
                 <v-list-item-content>
                     <v-list-item-title>
-                        E-Librars
+                        E-Library
 
                     </v-list-item-title>
                 </v-list-item-content>
