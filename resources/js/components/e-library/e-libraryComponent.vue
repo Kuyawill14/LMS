350b0a968d2bf0f0bd0d9f1ed06fc6548dd60f9d
markishere16 <markjoshua.mandigma@gmail.com>
2022-04-18 00:46:26 +0800

    update

--- resources/js/components/e-library/e-libraryComponent.vue
+++ resources/js/components/e-library/e-libraryComponent.vue
@@ -5,6 +5,8 @@
     User ID: isabelastateu
     <br>
     password: #library2021
+    <br>
+    <a href="https://search.ebscohost.com" target="_blank">Open in new tab</a>
    <iframe style="height:100vh" width="100%" src="https://search.ebscohost.com" title="Search Ebsco"></iframe>
 </div>
  
